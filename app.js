77447f05feca159f39cb2651044831338773f2c1
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-15 11:27:22 +0200

    simple dummy sign in

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { initViews } from "./view-router.js";
+import { initAuth, signIn, signOutUser } from "./simple-auth.js";
 
 window.addEventListener("load", initApp);
 
 function initApp() {
     console.log("app.js is running 🎉");
     initViews();
+    initAuth();
+    document.querySelector("#form-signin").addEventListener("submit", signIn);
+    document.querySelector("#btn-sign-out").addEventListener("click", signOutUser);
 }
